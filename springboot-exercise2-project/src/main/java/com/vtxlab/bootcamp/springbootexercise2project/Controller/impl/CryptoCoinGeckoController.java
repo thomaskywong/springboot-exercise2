9b07fcf7556da79d50092cc3b912c0e96aee478f
thomaskywong <thomaskywong@yahoo.com>
2024-02-29 23:34:15 +0800

    vue added

--- springboot-exercise2-project/src/main/java/com/vtxlab/bootcamp/springbootexercise2project/Controller/impl/CryptoCoinGeckoController.java
+++ springboot-exercise2-project/src/main/java/com/vtxlab/bootcamp/springbootexercise2project/Controller/impl/CryptoCoinGeckoController.java
@@ -5,10 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vtxlab.bootcamp.springbootexercise2project.Controller.CryptoCoinGeckoOperation;
 import com.vtxlab.bootcamp.springbootexercise2project.Service.CryptoGeckoService;
-import com.vtxlab.bootcamp.springbootexercise2project.Service.impl.RedisService;
 import com.vtxlab.bootcamp.springbootexercise2project.dto.jph.Coin;
 import com.vtxlab.bootcamp.springbootexercise2project.dto.jph.Market;
 import com.vtxlab.bootcamp.springbootexercise2project.exception.InvalidCurrencyException;
@@ -21,11 +19,6 @@ import com.vtxlab.bootcamp.springbootexercise2project.infra.Syscode;
 @RequestMapping(value = "/crypto/coingecko/api/v1")
 public class CryptoCoinGeckoController implements CryptoCoinGeckoOperation {
 
-  @Autowired
-  private RedisService redisService;
-
-  @Autowired
-  private ObjectMapper objectMapper;
 
   @Autowired
   private CryptoGeckoService cryptoGeckoService;
@@ -42,56 +35,10 @@ public class CryptoCoinGeckoController implements CryptoCoinGeckoOperation {
 
     List<Market> markets = cryptoGeckoService.getMarkets(cur, ids);
 
-
-    // List<Market> markets = new LinkedList<>();
-    // String key = "";
-    // String value = "";
-    // Market market;
-
-    // if (ids == null) {
-    //   try {
-    //     markets = cryptoGeckoService.getMarkets(cur);
-    //   } catch (RestClientException ex) {
-    //     throw new CoingeckoNotAvailableException(
-    //         Syscode.COINGECKO_NOT_AVAILABLE_EXCEPTION);
-    //   }
-
-
       return ApiResponse.<List<Market>>builder() //
           .ok() //
           .data(markets) //
           .build();
-
-    // } else {
-
-    //   try {
-    //     markets = cryptoGeckoService.getMarkets(cur, ids);
-    //   } catch (Exception ex) {
-
-    //   }
-
-    //   if (markets.size() == 0) {
-
-    //     for (String id : ids) {
-
-    //       if (!(CoinId.isValidCoinId(id))) {
-    //         throw new CoingeckoNotAvailableException(
-    //             Syscode.COINGECKO_NOT_AVAILABLE_EXCEPTION);
-    //       }
-
-    //       key = new StringBuilder("crytpo:coingecko:coin-markets:")
-    //           .append(currency).append(":").append(id).toString();
-    //       value = redisService.getValue(key);
-    //       market = objectMapper.readValue(value, Market.class);
-    //       markets.add(market);
-    //     }
-    //   }
-
-    //   return ApiResponse.<List<Market>>builder() //
-    //       .ok() //
-    //       .data(markets) //
-    //       .build();
-    // }
 
   }
 
